a07327f834548c6ca0184870a50b0c5a76b976d4
siddhu9009 <khotsiddharth96k@gmail.com>
2025-03-02 21:19:42 +0530

    Add files via upload

---
+++ employees_details/EMPOLYEE_PROJECT/src/main/java/com/example/EMPOLYEE_PROJECT/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.example.EMPOLYEE_PROJECT;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
---
+++ employees_details/EMPOLYEE_PROJECT/src/main/java/com/example/EMPOLYEE_PROJECT/EmpolyeeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.EMPOLYEE_PROJECT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpolyeeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpolyeeProjectApplication.class, args);
+	}
+
+}
